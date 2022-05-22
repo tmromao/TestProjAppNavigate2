3e01a5792c62e1ea746a1efd6423847b337817e9
tromao <tiagomromao@gmail.com>
2022-05-22 22:24:18 +0100

    Initial Commit

---
+++ TestProjAppNavigate2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProjAppNavigate2
+//
+//  Created by Tiago Miguel de Jesus Romao on 22/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProjAppNavigate2/TestProjAppNavigate2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjAppNavigate2App.swift
+//  TestProjAppNavigate2
+//
+//  Created by Tiago Miguel de Jesus Romao on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestProjAppNavigate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
