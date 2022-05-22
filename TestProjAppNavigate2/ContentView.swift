65307017b07acef0829662e757aab9346db3c089
tromao <tiagomromao@gmail.com>
2022-05-22 22:33:15 +0100

    Added the NaviationView for each tab items

--- TestProjAppNavigate2/ContentView.swift
+++ TestProjAppNavigate2/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem({
+                    Label("Home", systemImage: "house")
+                })
+            
+            SettingsView()
+                .tabItem({
+                    Label("Settings", systemImage: "gearshape")
+                })
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.mint
+                Text("Home Screen")
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct SettingsView: View{
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.pink
+                Text("Settings Screen")
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
